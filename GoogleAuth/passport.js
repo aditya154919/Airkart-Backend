1cf5799461a83649b8b37723f1c85df44097c212
user.name = Aditya <Aditya>
2025-12-06 19:07:28 +0530

    fix link

--- GoogleAuth/passport.js
+++ GoogleAuth/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/v1/auth/google/callback",
+      callbackURL: "https://airkart-backend.onrender.com/api/v1/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
